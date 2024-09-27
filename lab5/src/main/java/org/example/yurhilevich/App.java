107ad0620323cd95be7d2a21d020b68427a3018e
KL1cktm <urgzhenyok@mail.ru>
2024-09-27 16:43:44 +0300

    LabWork5 rework

--- lab5/src/main/java/org/example/yurhilevich/App.java
+++ lab5/src/main/java/org/example/yurhilevich/App.java
@@ -1,21 +1,28 @@
 package org.example.yurhilevich;
 
-/**
- * Hello world!
- *
- */
+import java.math.BigInteger;
+import java.util.Random;
+
 public class App 
 {
     public static void main( String[] args )
     {
+        Random random = new Random();
         RSAMethod method = new RSAMethod();
-        method.generatePrimeNumbers();
-        method.Encryption();
-//        method.digitalSignature();
-        System.out.println("\nДанные прочтённые из файла\n");
-        method.getSourceMessage();
-        method.getEncryptionMessage();
-        method.getOpenKey();
-        method.getSecretKey();
+        for (int i=1;i<11;i++) {
+            System.out.println("Тест " + i);
+            method.setValueM(Math.abs(random.nextInt()%1000000));
+            System.out.println("\nRSA метод\n");
+            method.encryption();
+            method.RSAEncryption();
+            BigInteger output = method.RSADecryption();
+            System.out.println("Результат расшифровки RSA: " + "\u001B[33m" +output + "\u001B[0m");
+            System.out.println("\nDS метод\n");
+            method.digitalSignature();
+            method.createDigitalSignature();
+            BigInteger output2 = method.signatureDecryption();
+            System.out.println("Результат расшифровки DS: " + "\u001B[33m" +output2 + "\u001B[0m");
+            System.out.println("\n\n");
+        }
     }
 }
--- lab5/src/main/java/org/example/yurhilevich/RSAMethod.java
+++ lab5/src/main/java/org/example/yurhilevich/RSAMethod.java
@@ -5,21 +5,20 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class RSAMethod {
-    private BigInteger p,q,n,fiN,d,m,c,s;
+    private BigInteger p, q, n, fiN, d, m, c, s;
     private BigInteger e = BigInteger.valueOf(65537);
 
-    public void generatePrimeNumbers(){
+    public void generatePrimeNumbers() {
         int bitLength = 1024;
         SecureRandom random = new SecureRandom();
-        this.p = BigInteger.probablePrime(bitLength,random);
-        this.q = BigInteger.probablePrime(bitLength,random);
+        this.p = BigInteger.probablePrime(bitLength, random);
+        this.q = BigInteger.probablePrime(bitLength, random);
 
-        while (this.p.equals(this.q)){
-            this.q = BigInteger.probablePrime(bitLength,random);
+        while (this.p.equals(this.q)) {
+            this.q = BigInteger.probablePrime(bitLength, random);
         }
 
         System.out.println("Первое сгенерированное простое число: " + this.p);
@@ -28,97 +27,125 @@ public class RSAMethod {
         System.out.println("Длина второго числа в битах: " + this.q.bitLength());
     }
 
-    public void interimCalculations(){
+    public void interimCalculations() {
         n = this.p.multiply(this.q);
-        BigInteger pMinusOne,qMinusOne;
+        BigInteger pMinusOne, qMinusOne;
         pMinusOne = this.p.subtract(BigInteger.ONE);
         qMinusOne = this.q.subtract(BigInteger.ONE);
 
         this.fiN = pMinusOne.multiply(qMinusOne);
         this.d = this.e.modInverse(this.fiN);
 
-        setValueM();
+//        setValueM();
     }
 
-    public void writeInFile(String filename,List<BigInteger> list){
+    public void writeInFile(String filename, List<BigInteger> list) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
-            for (BigInteger var: list) {
+            for (BigInteger var : list) {
                 writer.write(var.toString());
                 writer.newLine();
             }
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public List<BigInteger> readFromFile(String filename){
+    public List<BigInteger> readFromFile(String filename) {
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             List<BigInteger> list = new ArrayList<>();
             String line;
-            while ((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
                 list.add(new BigInteger(line));
             }
             return list;
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-    private void writeDataInFile(){
+    private void writeDataInFile() {
         List<BigInteger> list1 = new ArrayList<>();
         list1.add(this.e);
         list1.add(this.n);
-        writeInFile("open_key.txt",list1);
+        writeInFile("open_key.txt", list1);
         List<BigInteger> list2 = new ArrayList<>();
         list2.add(this.d);
         list2.add(this.n);
-        writeInFile("secret_key.txt",list2);
+        writeInFile("secret_key.txt", list2);
         list1.clear();
         list1.add(this.m);
-        writeInFile("source_message.txt",list1);
+        writeInFile("source_message.txt", list1);
     }
 
-    public void Encryption(){
+    public void RSAEncryption() {
+        List<BigInteger> list = getOpenKey();
+        List<BigInteger> list1 = getSourceMessage();
+        BigInteger m = list1.get(0);
+        BigInteger e = list.get(0);
+        BigInteger n = list.get(1);
+        BigInteger c = this.modularExponential(m, e, n);
+        System.out.println("Зашифрованное сообщение: " + c);
+        List<BigInteger> list2 = new ArrayList<>();
+        list2.add(c);
+        writeInFile("encryption_message.txt", list2);
+    }
+
+    public void encryption() {
+        generatePrimeNumbers();
         interimCalculations();
         writeDataInFile();
-        this.c = modularExponential(this.m,this.e,this.n);
-        List<BigInteger> list = new ArrayList<>();
-        list.add(this.c);
-        writeInFile("encryption_message.txt",list);
-        BigInteger outputM = modularExponential(this.c,this.d,this.n);
-        System.out.println("Открытый ключ (e): " + this.e);
-        System.out.println("Закрытый ключ (d): " + this.d);
-        System.out.println("Значение n: " + this.n);
-        System.out.println("Зашифрованное сообщение: " + this.c);
-        System.out.println("Расшифрованное сообщение: " + outputM);
+    }
+
+    public BigInteger RSADecryption() {
+        List<BigInteger> list = getSecretKey();
+        List<BigInteger> list1 = getEncryptionMessage();
+        BigInteger c = list1.get(0);
+        BigInteger d = list.get(0);
+        BigInteger n = list.get(1);
+        return modularExponential(c, d, n);
+    }
+
+    public BigInteger signatureDecryption() {
+        List<BigInteger> list = getOpenKey();
+        List<BigInteger> list1 = getEncryptionMessage();
+        BigInteger s = list1.get(0);
+        BigInteger e = list.get(0);
+        BigInteger n = list.get(1);
+        return modularExponential(s, e, n);
+    }
+
+    public void createDigitalSignature() {
+        List<BigInteger> list = getSecretKey();
+        List<BigInteger> list1 = getSourceMessage();
+        BigInteger m = list1.get(0);
+        BigInteger d = list.get(0);
+        BigInteger n = list.get(1);
+        BigInteger s = this.modularExponential(m, d, n);
+        System.out.println("Зашифрованное сообщение: " + s);
+        List<BigInteger> list2 = new ArrayList<>();
+        list2.add(s);
+        writeInFile("encryption_message.txt", list2);
     }
 
     public void digitalSignature() {
+        generatePrimeNumbers();
         interimCalculations();
-        this.s = modularExponential(this.m,this.d,this.n);
-        List<BigInteger> list = new ArrayList<>();
         writeDataInFile();
-        list.add(this.s);
-        writeInFile("encryption_message.txt",list);
-        BigInteger outputM = modularExponential(this.s,this.e,this.n);
-        System.out.println("Исходный ключ (e): " + this.e);
-        System.out.println("Секретный ключ (d): " + this.d);
-        System.out.println("Значение n: " + this.n);
-        System.out.println("Исходный текст (m): " + this.m);
-        System.out.println("Значение цифровой подписи s: " + this.s);
-        System.out.println("Прообраз подписи m*: " + outputM);
     }
 
-    public void setValueM(){
+    public void setValueM() {
         System.out.print("Введите число M: ");
         Scanner scanner = new Scanner(System.in);
         this.m = scanner.nextBigInteger();
     }
 
-    private BigInteger modularExponential(BigInteger base, BigInteger exp, BigInteger mod){
+    public void setValueM(int message){
+        BigInteger bigint = BigInteger.valueOf(message);
+        this.m = bigint;
+    }
+
+    private BigInteger modularExponential(BigInteger base, BigInteger exp, BigInteger mod) {
         BigInteger result = BigInteger.ONE;
         BigInteger baseTWO = BigInteger.valueOf(2);
         while (exp.compareTo(BigInteger.ZERO) > 0) {
@@ -131,29 +158,33 @@ public class RSAMethod {
         return result;
     }
 
-    public List<BigInteger> getEncryptionMessage(){
+    public List<BigInteger> getEncryptionMessage() {
+        System.out.println("Получение зашифрованного сообщения");
         List<BigInteger> list = readFromFile("encryption_message.txt");
         System.out.println("Зашифрованный текст: " + list.get(0));
         return list;
     }
 
-    public List<BigInteger> getOpenKey(){
+    public List<BigInteger> getOpenKey() {
+        System.out.println("Получение открытых ключей");
         List<BigInteger> list = readFromFile("open_key.txt");
         System.out.println("Исходный ключ (e): " + list.get(0));
         System.out.println("Исходное ключ (n): " + list.get(1));
         return list;
     }
 
-    public List<BigInteger> getSecretKey(){
+    public List<BigInteger> getSecretKey() {
+        System.out.println("Получение закрытых ключей");
         List<BigInteger> list = readFromFile("secret_key.txt");
         System.out.println("Секретный ключ (d): " + list.get(0));
         System.out.println("Секретный ключ (n): " + list.get(1));
         return list;
     }
 
-    public List<BigInteger> getSourceMessage(){
+    public List<BigInteger> getSourceMessage() {
+        System.out.println("Получение исходного сообщения");
         List<BigInteger> list = readFromFile("source_message.txt");
-        System.out.println("Исходный текст: " + list.get(0));
+        System.out.println("Исходный текст: " + "\u001B[33m" +list.get(0) + "\u001B[0m");
         return list;
     }
 }
